6196557e95a4c2b666d2c76577a17519209e8516
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-09 13:41:38 +0900

    Update Pset1-credit.c

--- Pset1-credit.c
+++ Pset1-credit.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 
 
+//power function
 
 long myPow(long x,long n)
 {
@@ -14,7 +15,10 @@ long myPow(long x,long n)
     return(number);
 }
 
-long checkSum(long num, long numLength) {
+//checksum function
+
+long checkSum(long num, long numLength)
+{
 
     long evenSum = 0;
     long oddSum = 0;
@@ -33,31 +37,28 @@ long checkSum(long num, long numLength) {
 
        evenSum += evenI;
 
-        printf("even: %li\n", evenI);
+
 
         } else {
 
             oddI = (num / myPow(10, i))%10;
 
+
             int oddItwo = oddI*2;
 
             if (oddItwo > 9) {
+
+                // if x 2 is double digit, add the two digits together
 
                 int oddItwox = (oddItwo / myPow(10, 0))%10;
                 int oddItwoxx = (oddItwo / myPow(10, 1))%10;
 
                 oddItwo = oddItwox + oddItwoxx;
- printf("oddITwo: %i\n", oddItwo);
             }
 
             oddSum += oddItwo;
 
-             printf("odd: %li\n", oddI);
-
         }
-
-
-
     }
 
     return evenSum + oddSum;
@@ -67,6 +68,8 @@ long checkSum(long num, long numLength) {
 
 int main(void) {
 
+    //get number input.
+
    long input;
 
    do {
@@ -74,12 +77,10 @@ int main(void) {
    }
    while(input < -1);
 
-
-
   long inputLength = 0;
-
   long inputOld = input;
 
+//while loop to find input digit length
 
    while (input > 0) {
     inputLength++;
@@ -88,36 +89,46 @@ int main(void) {
 
     long check = checkSum(inputOld, inputLength);
 
- printf("%li\n", inputLength);
- printf("%li\n", check);
+    //check if checksum is valid
 
+ if (check % 10 == 0) {
 
-if (inputLength == 15) {
-
+     //finds the nth digit in a number
 
   long first = (inputOld / myPow(10, inputLength - 1))%10;
   long second = (inputOld / myPow(10, inputLength - 2))%10;
 
-    if (first == 3) {
-        printf("valid!\n");
-    }
+  // validation for various types of cards
+
+     if (inputLength == 15) {
 
 
+         if (first == 3 && (second == 4 || second == 7)) {
+                printf("AMEX\n");
+            } else {
+                printf("INVALID\n");
+            }
 
+            } else if (inputLength == 16) {
 
-     printf("%li\n", first);
-      printf("%li\n", second);
+                if (first == 5 && (second == 1 || second == 2 || second == 3 || second == 4 || second == 5)) {
+                    printf("MASTERCARD\n");
+                } else if (first == 4) {
+                    printf("VISA\n");
+                }
+            } else if (inputLength == 13) {
+                 if (first == 4) {
+                    printf("VISA\n");
+                } else {
+                    printf("INVALID\n");
+                }
 
+            } else {
+                 printf("INVALID\n");
+            }
 
-  printf("%li\n", inputOld);
-
-}
-
-
-
-
-
-
-
+ } else  {
+   printf("INVALID\n");
+ }
 
 }
