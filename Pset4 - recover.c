4440a11b1707a08b942736a5a177a169f118d086
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-08-03 14:12:44 +0900

    Update Pset4 - recover.c
    Completed problem set

--- Pset4 - recover.c
+++ Pset4 - recover.c
@@ -5,8 +5,7 @@
 
 typedef uint8_t BYTE;
 
-bool start_of_new_jpeg(BYTE buffer);
-bool first_jpeg();
+bool start_of_new_jpeg(BYTE buffer1, BYTE buffer2, BYTE buffer3, BYTE buffer4);
 
 
 int main(int argc, char *argv[])
@@ -15,7 +14,7 @@ int main(int argc, char *argv[])
 
     if (argc < 2 || argc > 2)
     {
-        printf("File argument is absent.");
+        printf("Wrong number of file arguments.");
         return 1;
     } else
 
@@ -28,38 +27,54 @@ int main(int argc, char *argv[])
               return 1;
          } else
          {
+            BYTE buffer[512];
+            int jpeg = 0;
+            char filename[8];
+            FILE *img = NULL;
+            bool have_found_first_jpeg = false;
 
-
-             BYTE buffer[512];
-             while(fread(&buffer, 1, 512, file))
+             while(fread(&buffer, 512, 1, file) == 1)
              {
-                 if (start_of_new_jpeg(&buffer))
-                 {
-                     if (first_jpeg())
-                     {
-
-                     }
-                     else
-                     {
-
-                     }
-                 }
-                 else
+                 if (start_of_new_jpeg(buffer[0], buffer[1], buffer[2], buffer[3]) != 0)
                  {
 
+                     if (!have_found_first_jpeg)
+                     {
+                         have_found_first_jpeg = true;
+                     } else
+                     {
+                         fclose(img);
+                     }
+
+                        sprintf(filename, "%03i.jpg", jpeg);
+                        img = fopen(filename, "w");
+                        if (img == NULL)
+                        {
+                            return 1;
+                        }
+                        fwrite(buffer, 512, 1, img);
+                        jpeg++;
+                    } else if (have_found_first_jpeg)
+                 {
+                     fwrite(buffer, 512, 1, img);
                  }
+
+
              }
+             fclose(img);
+             fclose(file);
+
          }
     }
 
 
 }
 
-bool start_of_new_jpeg(BYTE buffer)
+bool start_of_new_jpeg(BYTE buffer1, BYTE buffer2, BYTE buffer3, BYTE buffer4)
     {
-        for (int i = 0; i < sizeof(buffer); i++)
-        {
 
-           return buffer[i] == 0xff && buffer[i+1] == 0xd8 && buffer[i+2] == 0xff && (buffer[i+3] & 0xf0) == 0xe0;
-        }
+
+           return buffer1 == 0xff && buffer2 == 0xd8 && buffer3 == 0xff && (buffer4 & 0xf0) == 0xe0;
+
     }
+
